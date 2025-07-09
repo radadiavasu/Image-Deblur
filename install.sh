ad46e1491f5e277ffc9ef9319b769cbb63b176bd
Vasu Radadia <114792837+radadiavasu@users.noreply.github.com>
2025-07-09 10:49:33 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Install BasicSR first without isolation to avoid __version__ KeyError
+# Install BasicSR first (outside requirements.txt) to avoid wheel build errors
 pip install --no-build-isolation git+https://github.com/XPixelGroup/BasicSR.git#egg=basicsr
 
-# Then install the rest of the project dependencies
+# Install the rest of the dependencies
 pip install -r requirements.txt
